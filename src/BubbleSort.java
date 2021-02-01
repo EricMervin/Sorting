6b55cc2ce71358105cc5923a20cd66b6fd6d7d36
ericmervin <ericmervin@192.168.1.4>
2021-02-01 10:42:43 +0530

    Bubble Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,9 +1,41 @@
 
 public class BubbleSort {
+	static void bubbleSort(int[] arr) {
+		int len = arr.length;
+		int tempInt = 0, i, j;
+		
+		for(i = 0; i < len; i++) {
+			for(j = 1; j < (len-i); j++) {
+				if(arr[j-1] > arr[j]) {
+					tempInt = arr[j-1];
+					arr[j-1] = arr[j];
+					arr[j] = tempInt;
+				}
+			}
+		}
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Program for bubble sort");
+		int intArr[] = {1,3,6,22,5,8,10,4,2};
+		int i;
+		
+		System.out.println("Array Before Sort: ");
+		
+		for(i = 0; i < intArr.length; i++) {
+			System.out.print(intArr[i] + " ");
+		}
+		
+		System.out.println();
+		
+		//Sorting the array elements
+		bubbleSort(intArr);
+		
+		System.out.println("Array After Sort: ");
+		
+		for(i = 0; i < intArr.length; i++) {
+			System.out.print(intArr[i] + " ");
+		}
 	}
 
 }
